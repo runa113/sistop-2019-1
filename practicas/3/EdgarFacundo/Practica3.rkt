1f821bdb0b317d34b656a0260ef8d1a2cd378d76
runa113 <facundo251292@hotmail.com>
2018-09-27 23:40:37 -0500

    Practica3
    Se suben archivos antes de ingresar .gitignore

---
+++ practicas/3/EdgarFacundo/Practica3.rkt
@@ -0,0 +1,16 @@
+#lang racket
+(define (f2 x)(* x x))
+(define pi 3.1416)
+
+;(define (met m)(* m 1 ))
+
+(define(cm m) (* m 100)) 
+
+;(define(dm m) (/ ( met m) 0.1))
+;(define(km m) (/ ( met m) 1000))
+;
+;
+;
+;
+;(define (f2 x)(* x x))
+;(define (f5 x)(*(f2 x)(f2 x) x)) 
